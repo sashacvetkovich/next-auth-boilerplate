47840df0ddda623e07d01ab008b033f6def01a40
Sasa Cvetkovic <sasa.cvetkovic@vegait.rs>
2024-06-27 15:42:10 +0200

    Added loading spinner

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,18 @@
 import { Suspense } from 'react';
+// Components
 import LoginForm from '@/components/auth/LoginForm';
+import LoadingSpinner from '@/components/shared/LoadingSpinner/LoadingSpinner';
 
 const LoginPage = () => {
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense
+        fallback={
+          <div className='flex justify-center mt-10'>
+            <LoadingSpinner />
+          </div>
+        }
+      >
         <LoginForm />
       </Suspense>
     </div>

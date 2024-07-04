430ff801414ee53161d624f891eda216f0ca648c
Sasa Cvetkovic <sasa.cvetkovic@vegait.rs>
2024-07-04 15:53:22 +0200

    Updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    domains: [
+      'lh3.googleusercontent.com',
+      'platform-lookaside.fbsbx.com',
+      'avatars.githubusercontent.com',
+    ],
   },
 };
 
